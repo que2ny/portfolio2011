a0e50f1cd9e4b81d5a3f6d72a9aa81b06397f10a
Hisun <hisun@connexiveinc.com>
2011-10-09 03:48:53 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio2011::Application.config.secret_token = 'f209b10f4d4203c733aa8e0d69422fe7fa595f9ca68440d933e4dafb37398876d744d7debcec1bd96ceda70d2d1a1e06e76effa91c0e8a60df0631a1864f60fd'
